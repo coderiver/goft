f5016c54cd3b8f0e83a33c26d5b42b491d6f8ba6
Ex-Zy <serega1905@ukr.net>
2016-09-29 17:07:56 +0300

    add popup and fix gallery

--- src/js/common.js
+++ src/js/common.js
@@ -1,14 +1,58 @@
 $(document).ready(function() {
 
-	// go js
-	$('.gallery__thumbs div').click(function(event) {
-		newlink = $(this).data('pic');
-		$('.gallery__main img').attr('src',newlink);
+	var gallery = $('.js-gallery-main'),
+		gallerySlide = gallery.find('.g-slide'),
+		galleryThumb = $('.js-gallery-thumb'),
+		galleryModal = $('.js-gallery-modal'),
+		galleryLarge = $('.js-gallery-large'),
+		galleryCounter = $('.js-gallery-counter'),
+		buttonClose = $('.js-close');
+
+	if (galleryModal.length) {
+		var clones = gallery.html();
+
+		galleryLarge.html(clones);
+
+		galleryLarge.slick({
+			prevArrow: '.js-gallery-prev',
+			nextArrow: '.js-gallery-next',
+			fade: true
+		});
+
+		galleryLarge.on('beforeChange', function(e, slick, current, next) {
+			galleryCounter.text(next + 1);
+		});
+
+		// galleryLarge.slick('slickGoTo', index);
+	}
+
+	gallery.slick({
+		arrows: false,
+		fade: true
 	});
 
-	$('.js-gallery-thumbs').slick({
-		infinite: true,
-		slidesToShow: 10,
-		slidesToScroll: 1
+	galleryThumb.click(function() {
+		var that = $(this);
+		var index = that.index();
+
+		gallery.slick("slickGoTo", index);
 	});
+
+	gallerySlide.click(function(){
+		var that = $(this);
+		var index = that.index();
+		
+		galleryCounter.text(index + 1);
+		galleryLarge.slick('slickGoTo', index);
+
+		if(!galleryModal.hasClass('is-open')) {
+			galleryModal.addClass('is-open');
+		}
+
+	});
+
+	buttonClose.click(function() {
+		galleryModal.removeClass('is-open');
+	})
+
 });
